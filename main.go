1f70d3a6aa0a79f4e4e670e67a4c5cd881792ccb
bogdannechyporenko <bnechyporenko@xebia.com>
2014-08-28 22:50:47 +0200

    Extracted the configuration to json file

--- main.go
+++ main.go
@@ -5,42 +5,79 @@ package main
  */
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/PuerkitoBio/goquery"
 	"encoding/json"
 	"os"
+
+	"io/ioutil"
 )
+
+type InputElement struct {
+	Source string
+	Find   string
+	Link   []string
+	Title  []string
+}
 
 type Article struct {
 	Link  string
 	Title string
 }
 
-func NewsScraper() {
-	doc, err := goquery.NewDocument("http://korrespondent.net/")
-	if err != nil {
-		log.Fatal(err)
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
+}
+
+func readInput() []InputElement {
+	var input = make([]InputElement, 0)
+	dat, err := ioutil.ReadFile("input.json")
+	check(err)
+	json.Unmarshal(dat, &input)
+	return input
+}
+
+func findElementValue(s *goquery.Selection, rules []string) string {
+
+	for i := 0; i < len(rules); i++ {
+		if rules[i] == "Find" {
+			i++
+			s = s.Find(rules[i])
+		} else if rules[i] == "Attr" {
+			i++
+			link, _ := s.Attr(rules[i])
+			return link
+		} else if rules[i] == "Text" {
+			return s.Text()
+		}
+	}
+
+	return ""
+}
+
+func NewsScraper() {
 
 	var articles = make([]Article, 0)
 
-	doc.Find(".time-articles .article__title").Each(func(i int, s *goquery.Selection) {
+	for _, inputElement := range readInput() {
 
-		link, _ := s.Find("a").Attr("href")
-		title := s.Find("a").Text()
+		doc, err := goquery.NewDocument(inputElement.Source)
+		check(err)
 
-		a := Article{link, title}
-		articles = append(articles, a)
-	})
+		doc.Find(inputElement.Find).Each(func(i int, s *goquery.Selection) {
+			link := findElementValue(s, inputElement.Link)
+			title := findElementValue(s, inputElement.Title)
 
-	b, err := json.MarshalIndent(articles, "", "  ")
-	if err != nil {
-		fmt.Println("error:", err)
+			a := Article{link, title}
+			articles = append(articles, a)
+		})
 	}
 
-	os.Stdout.Write(b)
+	json, err := json.MarshalIndent(articles, "", "  ")
+	check(err)
+
+	os.Stdout.Write(json)
 }
 
 func main() {
