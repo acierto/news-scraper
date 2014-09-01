57298942ec1ed2fef3cf6f9351a436b8a239b1e5
bogdannechyporenko <bnechyporenko@xebia.com>
2014-09-01 22:06:48 +0200

    Fixed auto-height for panels

--- web/js/controllers/mainController.js
+++ web/js/controllers/mainController.js
@@ -1,15 +1,42 @@
 angular.module('NewsScraperApp').controller('MainController',
-    function ($scope, ApiService) {
+    function ($scope, $interval, ApiService) {
 
-        ApiService.getArticles().then(function(articles) {
+        ApiService.getArticles().then(function (articles) {
             $scope.newsSources = articles.data;
         });
 
-        $scope.hasImage = function(article) {
+        $scope.hasImage = function (article) {
             return article.Img != '';
         };
 
-        $scope.openLink = function(link) {
+        $scope.openLink = function (link) {
+            $scope.$parent.selected = link;
             $("#external-article").html('<object data="' + link + '"/>');
+        };
+
+        var updateInternalPageContent = $interval(function () {
+            if (!_.isEmpty($('#external-article object'))) {
+                $interval.cancel(updateInternalPageContent);
+                $('#external-article object').css('height', calcHeight() + 'px');
+            }
+        }, 50);
+
+        function calcHeight() {
+            var originalHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+            return originalHeight - 20;
         }
+
+        function updateContentHeight() {
+            $('#left-column').css('height', calcHeight() + 'px');
+            $('#right-column').css('height', calcHeight() + 'px');
+            $('#external-article object').css('height', calcHeight() + 'px');
+        }
+
+        $scope.$on('$routeChangeSuccess', function () {
+            updateContentHeight();
+        });
+
+        $(window).resize(function () {
+            updateContentHeight();
+        }).trigger("resize");
     });
